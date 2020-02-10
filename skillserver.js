e27f670215e030493fff0e249a98f0f57ea24400
David Thaler <davidthaler@gmail.com>
2020-02-09 23:09:13 -0800

    GET, DELETE, PUT and POST methods work on skillserer. Tested with curl. Long polling not yet implemented, so you have to GET to see changes.

---
+++ skillserver.js
@@ -0,0 +1,112 @@
+const {createServer} = require('http')
+const Router = require('./router')
+const ecstatic = require('ecstatic')
+
+const router = new Router()
+const defaultHeaders = {'Content-Type': 'text/plain'}
+
+class SkillServer{
+    constructor(talks){
+        this.talks = talks
+        this.version = 0
+        this.waiting = []
+
+        let fileServer = ecstatic({root: './public'})
+        this.server = createServer((request, response) => {
+            let resolved = router.resolve(this, request)
+            if(resolved){
+                resolved.catch(error => {
+                    if(error.status != null) return error
+                    return {body: String(error), status:500}
+                }).then(({body, status=200, headers=defaultHeaders}) => {
+                    console.log(`${status} ${request.method} ${request.url}`)
+                    response.writeHead(status, headers)
+                    response.end(body)
+                })
+            }else{
+                fileServer(request, response)
+            }
+        })
+    }
+
+    start(port){
+        this.server.listen(port)
+    }
+
+    stop(){
+        this.server.close()
+    }
+}
+
+const talkPath = /^\/talks\/([^\/]+)$/;
+router.add('GET', talkPath, async (server, title) => {
+    if(title in server.talks){
+        return {
+            body: JSON.stringify(server.talks[title]),
+            headers: {'Content-Type': 'application/json'}
+        }
+    }else{
+        return {status: 404, body:`No talk found: ${title}`}
+    }
+})
+
+router.add('DELETE', talkPath, async(server, title) => {
+    if(title in server.talks){
+        delete server.talks[title]
+        // server.updated()
+    }
+    return {status: 204}
+})
+
+function readStream(stream){
+    return new Promise((resolve, reject) => {
+        let data = ''
+        stream.on('error', reject)
+        stream.on('data', chunk => data += chunk.toString())
+        stream.on('end', () => resolve(data))
+    })
+}
+
+router.add('PUT', talkPath, async(server, title, request) => {
+    let requestBody = await readStream(request)
+    let talk
+    try{
+        talk = JSON.parse(requestBody)
+    }catch (_){
+        return {status:400, body: 'Invalid JSON\n'}
+    }
+    if(!talk || 
+        typeof talk.presenter != 'string' || 
+        typeof talk.summary != 'string'){
+            return {status: 400, body: 'Bad talk\n'}
+        }
+    server.talks[title] = {title, 
+                            presenter:talk.presenter, 
+                            summary:talk.summary, 
+                            comments:[]}
+    // server.updated()
+    return {status: 204}
+})
+
+router.add('POST', talkPath, async(server, title, request) => {
+    let requestBody = await readStream(request)
+    let comment
+    try{
+        comment = JSON.parse(requestBody)
+    }catch(e){
+        return {status:400, body:'Invalid JSON'}
+    }
+    if(!comment || 
+        typeof comment.author != 'string' || 
+        typeof comment.message != 'string'){
+            return {status: 400, body: 'Bad Comment\n'}
+        }else if(title in server.talks){
+            server.talks[title].comments.push(comment)
+            // server.updated()
+            return {status: 204}
+        }else{
+            return {status:404, body:`No talk found: ${title}`}
+        }
+})
+
+new SkillServer({"smalltalk": {presenter:"Me", summary:"derp", comments:[]}}).start(8000)
